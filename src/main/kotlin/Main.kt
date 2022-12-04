9c12c34564d70fadbdf013610f76e638e94d3940
stinglucana <sting.lucana.ascornao@achmea.nl>
2022-12-04 18:31:49 +0100

    Updated 2016 input files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 import parser.inputCleaner
 import parser.parseFile
-import solutions._2016.calculateLitOnPixelsLittleScreen
+import solutions._2016.*
 import solutions._2022.*
 
 fun main(args: Array<String>) {
     val startTime = System.currentTimeMillis()
-    println(findOverlappingAssignments(inputCleaner(parseFile(2022, 4))))
+    println(findErrorCorrectedMessage(inputCleaner(parseFile(2016, 6))))
     println((System.currentTimeMillis() - startTime))
 }
