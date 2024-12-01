94ad457fb0e60eec98e35daf108b6641c080fbad
stinglucana <sting.lucana.ascornao@achmea.nl>
2024-12-01 07:59:46 +0100

    Migrate 2015 Day 5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 import day.Day
 import parser.inputCleaner
 import parser.parseFile
-import solutions._2024.Year2024Day1
+import solutions._2015.Year2015Day5
 
 fun main(args: Array<String>) {
     Runner().run(
-        Year2024Day1()
+        Year2015Day5()
     )
 }
 
---
+++ src/main/kotlin/solutions/_2015/Year2015Day5.kt
@@ -0,0 +1,69 @@
+package solutions._2015
+
+import day.Day
+
+class Year2015Day5 : Day {
+
+    override val year: Int = 2015
+    override val day: Int = 5
+
+    private val notAllowed = arrayOf("ab", "cd", "pq", "xy")
+    private val double = ('a'..'z').map { it.toString() + it }.toTypedArray()
+    private val vowels = arrayOf('a', 'e', 'i', 'o', 'u')
+
+    /**
+     * String that were NICE, not NAUGHTY
+     */
+    override fun part1(input: Sequence<String>): String {
+        fun String.containsAny(words: Array<String>) = words.any(this::contains)
+        fun String.containsThree(vowels: Array<Char>) = vowels.sumOf { this.count(it::equals) } >= 3
+
+        var niceStringsCounter = 0
+
+        for (string in input) {
+            if (
+                !string.containsAny(notAllowed)
+                && string.containsAny(double)
+                && string.containsThree(vowels)
+            ) {
+                niceStringsCounter++
+            }
+        }
+
+        return "$niceStringsCounter"
+    }
+
+    /**
+     * String that were NICE, not NAUGHTY
+     */
+    override fun part2(input: Sequence<String>): String {
+        var niceStringsCounter = 0
+
+        for (string in input) {
+            var firstRule = false // pair of letters that appears twice, no overlap
+            var secondRule = false // same letter with letter in between
+
+            for (i in 0..string.length - 3) {
+                val current = string[i]
+                val next = string [i + 1]
+                val afterNext = string[i + 2]
+
+                if (!secondRule) {
+                    secondRule = current == afterNext
+                }
+
+                if (!firstRule && i < string.length - 3) {
+                    val pair = current.toString() + next
+                    firstRule = string.substring(i + 2).contains(pair)
+                }
+
+                if (firstRule && secondRule) {
+                    niceStringsCounter++
+                    break
+                }
+            }
+        }
+
+        return "$niceStringsCounter"
+    }
+}
